4f49af7b079d63cc1a2af4731d970f2c0c06e092
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-18 21:42:21 -0700

    answers of calcTip2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+function calcTip(bill){
+    let tip;
+    if(bill>=50 && bill<=300){
+        tip=bill*0.15
+        console.log(`the bill was ${bill},the tip was${tip},and the total value${bill+tip}`);
+    }else if (bill>300){
+        tip=bill*0.2
+        console.log(`the bill was ${bill},the tip was ${tip},and the total value ${bill+tip}`);
+    }
+
+}
+console.log(calcTip(280))
